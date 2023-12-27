0dbe86b6b55b09f4b21ede3304cc1dbd57b32840
Rohit Atal <71523048+rohit9887@users.noreply.github.com>
2023-12-27 18:24:08 +0530

    Delete Library directory

--- Library/src/main/java/com/rohit/atal/Library/service/NotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.rohit.atal.Library.service;
-
-public class NotAvailableException extends Exception{
-    public NotAvailableException(String message){
-        super(message);
-    }
-}
